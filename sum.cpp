8988c1f7c5a1f19bee0509738d3791429f645e32
Ali Mousavi <mousavi.itbusiness@gmail.com>
2021-12-04 16:28:17 +0330

    Create sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int total = 0;
+	for (int i = 1; i < argc; ++i)
+		total += atoi(argv[i]);
+
+	cout << "Sum = " << total;
+
+	return 0;
+}
